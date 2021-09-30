c8259b0e780c9367c220c7a67803c68ddd4a61ea
Guilherme Daniel Rodrigues da Silva <guidanirs@gmail.com>
2021-09-30 11:28:42 -0300

    Projeto finalizado

---
+++ backend/src/main/java/com/devsuperior/dsclients/services/ClientService.java
@@ -0,0 +1,78 @@
+package com.devsuperior.dsclients.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsclients.dto.ClientDTO;
+import com.devsuperior.dsclients.entities.Client;
+import com.devsuperior.dsclients.repositories.ClientRepository;
+import com.devsuperior.dsclients.services.exceptions.DatabaseException;
+import com.devsuperior.dsclients.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client client = new Client();
+		copyDtoToEntity(client, dto);
+		client = repository.save(client);
+		return new ClientDTO(client);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client client = repository.getOne(id);
+			copyDtoToEntity(client, dto);
+			
+			client = repository.save(client);
+			return new ClientDTO(client);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+
+	private void copyDtoToEntity(Client client, ClientDTO dto) {
+		client.setName(dto.getName());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
+	}
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Guilherme', '05936464016', 200.0, '2003-09-01', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Pedro', '55273859000', 500.0, '2000-05-25', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Vinicius', '11846891051', 1000.0, '1998-10-10', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Marcelo', '22410465072', 5000.0, '1972-08-15', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Maria', '66716638075', 7000.0, '1990-07-18', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Matheus', '13717566008', 3500.0, '1995-01-20', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('João', '86016482072', 2500.0, '2001-01-03', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Mariana', '13755391082', 1500.0, '1999-12-22', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Marcelo', '78155916049', 9000.0, '1975-09-13', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Lucas', '12992362000', 6000.0, '1993-02-18', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_Date, children) VALUES ('Leonardo', '95135715053', 4300.0, '1994-06-17', 0);
